21047e976f6a32495b7b0ff55262dd4df11c9245
agsawyer <alyssa@vbm.com>
2023-05-26 16:00:38 -0700

    updated task3 code + comments

--- task3/task3updatedcode.py
+++ task3/task3updatedcode.py
@@ -5,11 +5,17 @@ import os
 import sys
 
 async def query(prompt):
+    '''takes in lmql prompt, returns model output'''
     output = (await lmql.run(prompt, output_writer=lmql.stream("RESPONSE")))
     return output
 
-async def main(filename):
+async def main():
+    # opening json file
+    with open(sys.argv[1], 'r') as f:
+        data = json.load(f)
     codes=data["codes"]
+
+    # getting total accuracy rate of questions from a quiz
     a = 0
     b = 0
     for c in codes:
@@ -19,10 +25,9 @@ async def main(filename):
                 a +=1
             b += 1
     accu_rate = a/b
+
+    # output 
     print(accu_rate)
 
 if __name__ == "__main__":
-    print(len(sys.argv))
-    with open(sys.argv[1], 'r') as f:
-        data = json.load(f)
-    asyncio.run(main(data))
+    asyncio.run(main())
