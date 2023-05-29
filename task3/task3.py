7d93960699a9af430fbac8ff2645a10832d6a996
agsawyer <alyssa@vbm.com>
2023-05-28 23:25:08 -0700

    updated task3 python script and bash

--- task3/task3.py
+++ task3/task3.py
@@ -1,54 +1,42 @@
+import os
 import lmql
 import asyncio
 import json
 import os
 import sys
 import csv
+from  pathlib import Path
 
-async def query(prompt):
-    '''takes in lmql prompt, returns model output'''
-    output = (await lmql.run(prompt, output_writer=lmql.stream("RESPONSE")))
-    return output
+async def query(c):
+    '''takes in lmql prompt, returns array of test results'''
+    correctAnswer = "\n\n" + c["answer"]
+    output = (await lmql.run(c["code"], output_writer=lmql.stream("RESPONSE")))
+    if output[0].variables['ANSWER'] == correctAnswer:
+        return 1
+    return 0
 
 async def main():
+    # printing csv column names: model, quiz chapter, prompt style, and accuracy rate
+    print("model, quizCh, promptStyle, accuracy")
+
     # opening json file
-    # TODO: edit to be more generalized
-    jsonName = sys.argv[1]
-    with open(jsonName, 'r') as f:
-        data = json.load(f)
-    codes=data["codes"]
+    directory = sys.argv[1]
+    for file_path in (Path.cwd()/directory).glob("*.json"):
 
-    # getting total accuracy rate of questions from a quiz
-    correct = 0
-    for c in codes:
-            response = await query(c["code"])
-            answer_from_Output = response[0].variables['ANSWER']
-            if answer_from_Output == ("\n\n" + c["answer"]):
-                correct +=1
-    accu_rate = correct/len(codes)
+        with file_path.open(mode='r') as f:
+            data = json.load(f)
+        codes=data["codes"]
 
-    # output 
-    print(accu_rate)
-    toCSV(jsonName, accu_rate)
-
-def toCSV(jsonName, accu_rate):
-    '''adds info to quiz type csv'''
-    #TODO: get from folder name
-    quiztype = "test"
+        # getting total accuracy rate of questions from a quiz
+        results = await asyncio.gather(*[query(c) for c in codes])
+        accuracy = sum(results)/len(results)
     
-    f = open('test.txt', 'w+') 
-    f.write('python rules')
-    f.close()    
+        # parse json file
+        infoList = file_path.stem.split("_")
     
-    infoList = jsonName.split("_")
-    # add all of that info + acc_rate to a new line in the csv file
-    with open("quiztype" + quiztype + ".csv", 'w+') as f:
-        writer = csv.writer(f)
-        # write to csv with columns as:  model, quiz chapter, prompt style, and accuracy rate
-        writer.writerow([infoList[2].replace(".json",""), infoList[0], infoList[1], accu_rate])
-        f.flush()
-
-    print("dne?")
+        # printing row of csv file
+        output = [infoList[2], infoList[0], infoList[1], str(accuracy)]
+        print(", ".join(output))
 
 if __name__ == "__main__":
     asyncio.run(main())
--- task3/task3_run.sh
+++ task3/task3_run.sh
@@ -1,4 +1,5 @@
-for filename in *.json; do
-    python3 task3csvdebug.py "$filename"
-
+for dir in quizType*; do
+    if [ -d "$dir" ]; then
+	python3 task3.py "$dir" > "$dir".csv
+    fi
 done
