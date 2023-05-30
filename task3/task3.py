3d8e3d1113781642b24de4febc311866237b246f
agsawyer <alyssa@vbm.com>
2023-05-30 13:52:53 -0700

    making main in task3.py non async

--- task3/task3.py
+++ task3/task3.py
@@ -1,4 +1,3 @@
-import os
 import lmql
 import asyncio
 import json
@@ -8,35 +7,42 @@ import csv
 from  pathlib import Path
 
 async def query(c):
-    '''takes in lmql prompt, returns array of test results'''
+    '''takes in a dict that includes lmql prompt and the true answer,
+    returns 1 if the model is correctand 0 if wrong'''
+    # formating answer to use to test exact match
     correctAnswer = "\n\n" + c["answer"]
+
+    # querying lmql 
     output = (await lmql.run(c["code"], output_writer=lmql.stream("RESPONSE")))
     if output[0].variables['ANSWER'] == correctAnswer:
         return 1
     return 0
 
-async def main():
-    # printing csv column names: model, quiz chapter, prompt style, and accuracy rate
+def calcAccuracy(data):
+    '''calculates accuracy based on outputs from query'''
+    codes=data["codes"]
+    
+    # using query to prompt model with questions in parallel 
+    loop = asyncio.get_event_loop()
+    results = loop.run_until_complete(asyncio.gather(*[query(c) for c in codes]))
+    loop.close()
+
+    return sum(results)/len(results)
+
+def main():
+    # printing csv column names
     print("model, quizCh, promptStyle, accuracy")
 
     # opening json file
     directory = sys.argv[1]
     for file_path in (Path.cwd()/directory).glob("*.json"):
-
         with file_path.open(mode='r') as f:
             data = json.load(f)
-        codes=data["codes"]
 
-        # getting total accuracy rate of questions from a quiz
-        results = await asyncio.gather(*[query(c) for c in codes])
-        accuracy = sum(results)/len(results)
-    
-        # parse json file
+        # printing string of what will be a row of the csv file
         infoList = file_path.stem.split(".")
-    
-        # printing row of csv file
-        output = [infoList[2], infoList[0], infoList[1], str(accuracy)]
+        output = [infoList[2], infoList[0], infoList[1], str(calcAccuracy(data))]
         print(", ".join(output))
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
