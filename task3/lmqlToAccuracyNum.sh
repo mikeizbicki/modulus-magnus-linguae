90c9fdb6f1d97ca510c606bc1f05edc48bd2c3a1
Alyssa Sawyer <alyssa@vbm.com>
2023-06-02 13:57:55 -0700

    Delete task3 directory

--- task3/lmqlToAccuracyNum.sh
+++
@@ -1,5 +0,0 @@
-for dir in quizType*; do
-    if [ -d "$dir" ]; then
-	python3 lmqlToAccuracyNum.py "$dir"
-    fi
-done
--- task3/lmqlToCsv.sh
+++
@@ -1,5 +0,0 @@
-for dir in quizType*; do
-    if [ -d "$dir" ]; then
-	python3 task3.py "$dir" > "$dir".csv
-    fi
-done
